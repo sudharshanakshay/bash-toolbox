b7ff1e588a1b039dc4db40c2eb10c4958324227e
Sudharshan Akshay <49453742+sudharshanakshay@users.noreply.github.com>
2025-03-11 18:54:29 +0530

    Add files via upload

---
+++ remove_copy_suffix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#read -p "Enter the directory path: " directory
+
+directory="/home/ohmkar/Pictures/Ufufigigifyffygdownload/"
+
+if [[ ! -d "$directory" ]]; then
+echo "Directory does not exists."
+exit 1
+fi
+
+for file in "$directory"/*; do
+if [[ "$file" == *"(1)"* ]]; then
+new_name=$(echo "$file" | sed 's/ (1)//g' )
+mv "$file" "$new_name"
+echo "Renamed: $file -> $new_name"
+echo ""
+fi
+done
